bcb5a9537ac3fa3a56f434fb1e4deac970d70abc
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2022-12-05 01:25:37 +0330

    fix bug

--- chapter_2.py
+++ chapter_2.py
@@ -6,7 +6,7 @@ X_train = np.array([
     [0, 0, 0],
     [1, 1, 0]])
 Y_train = ['Y', 'N', 'Y', 'Y']
-X_test = np.array([1, 1, 0])
+X_test = np.array([[1, 1, 0]])
 
 
 def get_label_indices(labels):
